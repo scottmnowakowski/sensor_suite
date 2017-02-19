544c5cfa6ba5ca332c7a9a54a22d0096a96be158
scottmnowakowski <scottmnowakowski@gmail.com>
2017-02-19 13:46:13 -0500

    Changed arguments for I2C1 functions.

--- src/I2C1.h
+++ src/I2C1.h
@@ -1,3 +1,3 @@
 void init_I2C1();
-void readBytes_I2C1();
-void writeBytes_I2C1();
+void readBytes_I2C1(uint8_t addr, uint8_t reg, uint8_t size, uint8_t *buf);
+void writeBytes_I2C1(uint8_t addr, uint8_t reg, uint8_t size, uint8_t *buf);
